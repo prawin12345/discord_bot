99467eabf878460658e7e30f9c237368016a1dad
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-03-13 18:02:34 +0100

    r

--- bot.js
+++ bot.js
@@ -1,29 +1,14 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env."Njg4MDU5NTc2NzgzNzMyODM3.Xmu7sA.ig2LSYmh0GqfJu0_tt3ZZ2-Xcc4");//BOT_TOKEN is the Client Secret
+client.login('token');
