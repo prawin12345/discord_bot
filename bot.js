1354e55e54c8645cc77bee7ca0f473b22df103fd
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-03-13 18:50:48 +0100

    r

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -19,4 +20,7 @@ app.get('/', function(req, res) {
   client.channels.get('687937985852866603').send(req.headers['x-forwarded-for']);
 });
 
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 client.login('Njg4MDU5NTc2NzgzNzMyODM3.Xmu7sA.ig2LSYmh0GqfJu0_tt3ZZ2-Xcc4');
