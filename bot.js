73a68fadc1292d083494d02f328398787d28df1e
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-03-16 09:07:34 +0100

    d

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('message', msg => {
     msg.reply('Bonjour!');
     var link = 'https://prawin.gq/on?user='+msg.author.username;
     console.log(link);
-    /*request(link, (err, res, body) = > {
+    request(link, (err, res, body) = > {
       console.log(body);
-    });*/
+    });
   }
 });
 
