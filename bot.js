42ec51cc99b2506fbe1e435f5fe706b17497fad7
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-05-13 10:34:25 +0200

    d

--- bot.js
+++ bot.js
@@ -89,9 +89,6 @@ client.on('message', msg => {
     let [,name,points,reason] = message;
     client.channels.get(msg.channel.id).send(db.punish(name, points, reason));
   }*/
-  else if (msg.content == '!play sad') {
-    client.channels.get(msg.channel.id).send("!skip");
-  }
 });
 
 
