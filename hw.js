de8cbb0ba75b1331e39a24ef022026424bcfb207
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-05-18 20:41:13 +0200

    d

--- hw.js
+++ hw.js
@@ -3,20 +3,33 @@ function modifyHomework(msg, subject, input, type) {
     fs = require('fs');
     fs.readFile(__dirname+'/hw.json', (err, file) => {
         if (err) return msg.reply(err);
-        var hw = JSON.parse(file)
-        console.log(hw);
-        console.log(arguments);
-        subject = hw[subject];
-        if (subject == null) return msg.reply("Hausaufgabe mit diesem Fach nicht gefunden.");
+        var hw = JSON.parse(file);
+        if (hw[subject] == null) return msg.reply("Hausaufgabe mit diesem Fach nicht gefunden.");
         else if (input == null) return msg.reply("Unvollständige Eingabe");
-        type = type || "do";
-        if (type !== "do" && type !== "for") return msg.reply("Ungültige Eingabe für Datentyp");
+        type = type || "doTime";
+        if (type !== "doTime" && type !== "forTime") return msg.reply("Ungültige Eingabe für Datentyp");
         hw[subject][type] = input;
         fs.writeFile(__dirname+'/hw.json', JSON.parse(hw, null, '\t'), (err) => {
             if (err) return msg.reply(err);
         });
-        console.log(hw);
         return msg.reply(JSON.parse(hw, null, '\t'));
     })
 }
+
+function addHomework(msg, subject, forTime, doTime) {
+    fs = require('fs');
+    fs.readFile(__dirname+'/hw.json', (err, file) => {
+        if (err) return msg.reply(err);
+        var hw = JSON.parse(file);
+        if (subject == null || forTime == null || doTime == null) return msg.reply("Unvollständige Eingabe");
+        if (hw[subject] !== null) return msg.reply("Fach existiert schon");
+        hw[subject] = {forTime: forTime, doTime: doTime}
+        fs.writeFile(__dirname+'/hw.json', JSON.parse(hw, null, '\t'), (err) => {
+            if (err) return msg.reply(err);
+        })
+        return msg.reply(JSON.parse(hw, null, '\t'));
+    })
+}
+
 exports.modifyHomework = modifyHomework;
+exports.addHomework = addHomework;
