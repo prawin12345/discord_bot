f7159f4dcc033c8441f4b0a7e298d8507be70369
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-05-24 16:40:36 +0200

    d

--- hw.js
+++ hw.js
@@ -23,7 +23,7 @@ function addHomework(msg, subject, forTime, doTime) {
         if (err) return console.log(err);
         var hw = JSON.parse(file);
         if (subject == null || forTime == null || doTime == null) return msg.reply("Unvollständige Eingabe");
-        if (hw[subject] !== null) { console.log(subject, hw[subject]); return msg.reply("Fach existiert schon");}
+        if (hw[subject] === undefined) { console.log(subject, hw[subject]); return msg.reply("Fach existiert schon");}
         hw[subject] = {forTime: forTime, doTime: doTime}
         fs.writeFile(__dirname+'/hw.json', JSON.stringify(hw, null, '\t'), (err) => {
             if (err) return console.log(err);
