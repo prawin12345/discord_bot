6a7a0a4b7899bfbbf7a101b241e32a583531410f
Prawin Rajasena <prawinrajasena@Prawin-3.local>
2020-05-25 15:07:08 +0200

    d

--- hw.js
+++ hw.js
@@ -84,7 +84,7 @@ function getData() {
     queryStr = `SELECT "data" FROM "Json" WHERE "name" = 'hw';`;
     client.query(queryStr, (err, res) => {
         if (err) console.log(err);
-        if (res) {console.log(res.rows[0].data); return res.rows[0].data; /*return res.rows[0].data;*/}
+        if (res) {return res.rows[0].data; /*return res.rows[0].data;*/}
     });
 }
 
